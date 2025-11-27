7308eaf635d3a8cc8a1c87e376c27ba34024dc4b
Nicolas Battaglia <nicobattmdq@gmail.com>
2025-11-27 16:00:15 -0300

    Add multiple greets

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -22,3 +22,15 @@ func randomFormat() string {
 	}
 	return formats[rand.Intn(len(formats))]
 }
+
+func Hellos(names []string) (map[string]string, error) {	
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
--- hello/hello.go
+++ hello/hello.go
@@ -10,10 +10,14 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
     log.SetFlags(0)
+
+	names := []string{"Nico", "Matías", "Gabi"}
 	
-    message, err := greetings.Hello("Nico")
+    message, err := greetings.Hellos(names)
+	for _, greet := range message {
+		fmt.Println(greet)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-    fmt.Println(message)
 }
